fdd732752ddbe2e7a75d774f4682bb7fb668ba0f
h t <me@h.t>
2023-05-04 13:24:16 +0530

    binary search

---
+++ src/code/binarySearch.ts
@@ -0,0 +1,23 @@
+/**
+ * Assuming the dataset is ordered
+ * taking the search to next level :p
+ */
+
+export default function binarySearch(
+  haystack: number[],
+  needle: number
+): boolean {
+  let lo = 0
+  let hi = haystack.length
+
+  do {
+    const mid = Math.floor(lo + (hi - lo) / 2)
+    const val = haystack[mid]
+
+    if (val === needle) return true
+    else if (val > needle) hi = mid
+    else lo = mid + 1
+
+  } while (lo < hi)
+  return false
+}
